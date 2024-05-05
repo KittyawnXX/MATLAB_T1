7ea17bf80a4e8ed76bdf1f9afdf3dd4d05dd0700
Kittyawn <aviatrix0120@gmail.com>
2024-05-05 07:04:30 -0700

    Update s2.m

--- Tugas/S2 Pembangkit Sinyal Waktu Diskrit/s2.m
+++ Tugas/S2 Pembangkit Sinyal Waktu Diskrit/s2.m
@@ -2,7 +2,7 @@ k=0;
 n1=-5;
 n2=5;
 n =[n1:n2];
-x =[(n-k) == 0];    % the value will be 1 when (n-k)=0, otherwise it'll stay on 0 value
+x =[(n-k) >= 0];    % the value will be 1 when (n-k)=0, otherwise it'll stay on 0 value
 stem(n,x);
 
 title('Sinyal Cuplik Satuan')
